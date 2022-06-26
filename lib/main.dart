f9547dbb153f6994a1fc9ace7d7a7971e5e217e1
alisiddiqui03 <90140854+alisiddiqui03@users.noreply.github.com>
2022-06-26 15:33:14 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Scaffold(appBar: AppBar(
+  backgroundColor: Colors.black,
+title: Text('HEY!! IM HERE GUYS'),
+centerTitle: true,
+),
+body:
+Center(
+child:   
+Image (image: 
+NetworkImage('https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2Fyc3xlbnwwfHwwfHw%3D&w=1000&q=80')
+,))
+   
+   
+   
+    
+   )
+  
+
+
+));
